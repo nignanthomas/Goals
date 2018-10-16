2d9110407d479cadb4c7378f977837119ec9fd7a
nignanthomas <nignanthomas@gmail.com>
2018-10-16 09:01:58 +0300

    monday work - create component, classes, binding, interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailsComponent
   ],
   imports: [
     BrowserModule
