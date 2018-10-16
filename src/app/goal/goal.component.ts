057511d1975113fb3a92a2ac119811ba4bee1d26
nignanthomas <nignanthomas@gmail.com>
2018-10-16 10:02:35 +0300

    output property binding - add toggle button to display/hide description of goals

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -17,6 +17,10 @@ export class GoalComponent implements OnInit {
     new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
   ]
 
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
   constructor() { }
 
   ngOnInit() {
