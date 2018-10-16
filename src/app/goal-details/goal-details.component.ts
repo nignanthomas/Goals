78f2625e2c672fb7347790ed30b6d1586aa2a34c
nignanthomas <nignanthomas@gmail.com>
2018-10-16 14:42:03 +0300

    event emitters - line-through on click and none on dblclick; display goal completion date

--- src/app/goal-details/goal-details.component.ts
+++ src/app/goal-details/goal-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Goal} from "../goal"
 @Component({
   selector: 'app-goal-details',
@@ -8,6 +8,11 @@ import {Goal} from "../goal"
 export class GoalDetailsComponent implements OnInit {
 
   @Input() goal:Goal;
+  @Output () isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
